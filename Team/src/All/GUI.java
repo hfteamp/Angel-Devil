e4c254fa4b32ef4fbe9ee5738ccb56de19ed9be2
김태현 <@DESKTOP-3J7GPC7>
2019-06-10 00:04:58 +0900

    Piece 구현완료(성훈이 GUI 살짝 만짐 )

--- Team/src/All/GUI.java
+++ Team/src/All/GUI.java
@@ -1,58 +1,170 @@
 package All;
 
-import java.awt.event.*;
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GUI {
-	JFrame frame;
-	JPanel board[][] = new JPanel[8][8];
-	JButton piecebutton[] = new JButton[20];
-	JPanel iu;
-	JTextArea chat;
-	JTextField input;
-	JButton submit;
-	JPanel deathzone[];
-	JButton start;
-	int sql = 100;
-	int click = 0;
-	int pid = 0;
-	int px, py;
-	String Turn = "Devil";
-	String pteam;
-
-	class BoardListener { // implement MouseListener
-		int x, y;
-
-		void BoardListener(int x, int y) {
-
-		}
-
-		void mouseClicked(MouseEvent e) {
-
-		}
+public class GUI extends JFrame{
+	static JFrame frame;
+	static JPanel board[][]=new JPanel[7][7];
+	static JButton piecebutton[]=new JButton[14];
+	static JPanel iu;
+	static JTextArea chat;
+	static JTextField input;
+	static JButton submit;
+	static JPanel deathzone;
+	static JButton start = new JButton("시작");
+	int sql=100;
+	static int click=0;
+	static int pid;
+	static int px,py;
+	static String pteam;
+	static String turn="Devil";
+	static Piece piece[] = new Piece[14];
+	static Piece p;
+	static ImageIcon icon = new ImageIcon("image/Angel.png");
+	static ImageIcon icon2 = new ImageIcon("image/Devil.png");
+	static int gaming;
+		
+		
+	class StartListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e){
+        	gaming=1;
+			}
 	}
+	
+	
+	class BoardListener implements MouseListener{   
+		int x,y;
+		
+		BoardListener(int a, int b){
+             this.x=a;
+             this.y=b;
+             }
+		public void mouseClicked(MouseEvent e) {
+			if(gaming==1) piece[pid].move(x,y); // 말이동
+   	 
+        }
+		public void mousePressed(MouseEvent e) {
+	    }
 
-	class PieceListener { // implement MouseListener
-		int x, y;
-		int id;
-		String team;
+	    @Override
+	    public void mouseReleased(MouseEvent e) {
+	    }
 
-		void PieceListener(int x, int y, String team) {
+	    @Override//
+	    public void mouseEntered(MouseEvent e) {
 
+	    }
+
+	    @Override//
+	    public void mouseExited(MouseEvent e) {
+
+	    }
+}
+	class PieceListener implements ActionListener{   
+		int x, y, id; 
+		String team; 
+
+   	
+        PieceListener(int x, int y,int id,String team){
+            this.x=x;
+            this.y=y;
+            this.id=id; // ���� �ĺ���ȣ
+            this.team = team;
+       	 }
+        public void actionPerformed(ActionEvent e){
+        	
+        	if(gaming==1) piece[id].kill(); // 말잡기
+        	if(gaming==1) piece[id].postpiece(); // 말상태저장
+        	if(gaming==0) piece[id].trans(id); //말변경( 미 구현)
+			}
+			 	}
+
+        
+	
+	
+	public void addboard() {
+		int aa=0;
+		for(int x=0; x<7; x++) {
+			for(int y=0; y<7; y++) {
+				board[x][y] = new JPanel();
+				if(x%2==0&&y%2==0) {
+					board[x][y].setBackground(Color.black);
+				}else if(x%2!=0&&y%2==0) {
+					board[x][y].setBackground(Color.white);
+				}else if(x%2==0&&y%2!=0) {
+					board[x][y].setBackground(Color.white);
+				}else if(x%2!=0&&y%2!=0) {
+					board[x][y].setBackground(Color.black);
+				}
+				if(x==0) {
+					piecebutton[aa].setIcon(icon);
+					board[x][y].add(piecebutton[aa]);
+					piecebutton[aa].addActionListener(new PieceListener(x,y,aa,"Angel"));
+					piece[aa] = new Piece(x, y, aa,"Angel");
+					aa++;
+				}
+				if(x==6) {
+					piecebutton[aa].setIcon(icon2);
+					board[x][y].add(piecebutton[aa]);
+					piecebutton[aa].addActionListener(new PieceListener(x,y,aa,"Devil"));
+					piece[aa] = new Piece(x, y, aa, "Devil");
+					aa++;
+				}
+				board[x][y].setLocation(y * sql,x* sql+40); 
+				board[x][y].setSize(sql, sql);
+				board[x][y].setLayout(new FlowLayout());
+				board[x][y].addMouseListener(new BoardListener(x,y));
+				
+				frame.add(BorderLayout.CENTER,board[x][y]);
+			}
 		}
-
-		void ActionPerformed(ActionEvent e) {
-
-		}
+		start.addActionListener(new StartListener());
+		addgui();
 	}
-
-	void AddBoard() {
-
+	
+	public void addpiece() {
+		frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		
+		for(int i=0; i<14; i++) {
+			piecebutton[i] = new JButton();
+			piecebutton[i].setPreferredSize(new Dimension(80, 80));
+			piecebutton[i].setBorderPainted(false); 
+			piecebutton[i].setFocusPainted(false);  
+			piecebutton[i].setContentAreaFilled(false);
+			
+			
+			
+			
+			
+			
+		
 	}
+		addboard();
 
-	void AddPiece() {
-
+}
+	
+	public void addgui() {
+		iu= new JPanel();
+		iu.setLayout(null);
+		iu.setBackground(Color.GRAY);
+		
+		start.setBounds(800, 500, 100, 100);
+		frame.add(start);
+		frame.add(iu);
+		frame.setSize(1000, 1000);
+		frame.setTitle("Angel and Devil Game");
+		frame.setVisible(true);
+		
+		
 	}
+	
 
+	
+	public static void main(String[] args) {
+		GUI gui=new GUI();
+		gui.addpiece();
+	}
 }
--- Team/src/All/Piece.java
+++ Team/src/All/Piece.java
@@ -1,19 +1,132 @@
 package All;
 
-public class Piece {
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class Piece extends GUI {
 	int x, y, id, state;
 	String team;
 	int ascore, dscore;
+	static int count=0,count2=0;
+	Piece(int x,int y,int id,String team) {
+		this.x = x;
+		this.y = y;
+		this.id = id;
+		this.team = team;
+	}
+	void postpiece() {
+		if (click == 0) {
+			if (team == turn) {
+				px = this.x;
+				py = this.y;
+				pid = this.id;
+				pteam = this.team;
+				click++;	
+				piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + "_clik.png"));
 
-	void move(int x, int y) {
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "순서가 아닙니다.");
+			}
+    	}
+
+
 
 	}
 
-	void kill(String team, int state) {
+	public void move(int x, int y) {
+		
+		if (click == 1) {
+			if (((px - 1 == x || x == px + 1) && (py - 1 != y && y != py + 1))
+					|| ((py - 1 == y || y == py + 1) && (px - 1 != x && x != px + 1))) {
+				board[x][y].add(piecebutton[pid]);
+				piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
+				board[px][py].removeAll();
+				board[px][py].revalidate();
+				board[px][py].repaint();
+				piece[pid].x = piece[pid].x - (piece[pid].x - x);
+				piece[pid].y = piece[pid].y - (piece[pid].y - y);
+				click = 0;
+				if (turn == "Devil") {
+					turn = "Angel";
+				} else {
+					turn = "Devil";
+				}
+
+			} else {
+				JOptionPane.showMessageDialog(null, "범위 아웃");
+				piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
+				click = 0;
+
+			}
+
+		} else {
+			JOptionPane.showMessageDialog(null, "말을 먼저 선택");
+			click = 0;
+		} 
+		
+		
+	
 
 	}
 
-	void trans(int id, int state) {
+	void kill() {
+
+	if (click == 1) {
+				if (team == turn) {
+					JOptionPane.showMessageDialog(null, "자신의 말은 잡을수 없습니다.");
+					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
+					click = 0;
+				} else if (((px - 1 == piece[id].x || piece[id].x == px + 1)
+						&& (py - 1 !=  piece[id].y &&  piece[id].y != py + 1))
+						|| ((py - 1 ==  piece[id].y ||  piece[id].y == py + 1)
+								&& (px - 1 !=  piece[id].x &&  piece[id].x != px + 1))) {
+
+					board[piece[id].x][piece[id].y].removeAll();
+					board[piece[id].x][piece[id].y].revalidate();
+					board[piece[id].x][piece[id].y].repaint();
+					board[piece[id].x][piece[id].y].add(piecebutton[pid]);
+					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
+					board[piece[pid].x][piece[pid].y].removeAll();
+					board[piece[pid].x][piece[pid].y].revalidate();
+					board[piece[pid].x][piece[pid].y].repaint();
+
+					piece[pid].x = piece[id].x;
+					piece[pid].y = piece[id].y;
+					click = 0;
+					
+					if (turn == "Devil") {
+						turn = "Angel";
+					} else {
+						turn = "Devil";
+					}	
+				
+				} else {
+					JOptionPane.showMessageDialog(null, "이동범위 벗어났습니다.");
+					piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
+					click = 0;
+				}
+
+			}
+		
+
+	}
+
+	void trans(int id) {
+		
+	
+		/*if(id<7) {
+			if(count<3) {
+		piecebutton[id].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
+		count++;
+			}
+		}else {
+			if(count2<3) {
+			piecebutton[id].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
+			count2++;
+			}
+		}*/
+		
 
 	}
 }
