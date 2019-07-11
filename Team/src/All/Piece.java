ddd951d15d6c8f9818f6c8fa498e0218567e4d79
STUDYROOM3 <STUDYROOM3@DESKTOP-RMC3091>
2019-07-11 14:16:01 +0900

    kill

--- Team/src/All/Piece.java
+++ Team/src/All/Piece.java
@@ -13,9 +13,10 @@ public class Piece extends GUI {
 		this.x = x;
 		this.y = y;
 		this.id = id;
-		this.state=state;
+		this.state = state;
 		this.team = team;
 	}
+
 	void postpiece() {
 		if (click == 0) {
 			if (team == turn) {
@@ -23,38 +24,30 @@ public class Piece extends GUI {
 				py = this.y;
 				pid = this.id;
 				pteam = this.team;
-				click++;	
+				click++;
 				piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + "_clik.png"));
 
-
-				
-			}else if(killed==1){
+			} else if (killed == 1) {
 				killed = 0;
-				
-			}else {
+
+			} else {
 				JOptionPane.showMessageDialog(null, "순서가 아닙니다.");
 			}
-    	}
-
-
+		}
 
 	}
 
 	public void move(int x, int y) {
-		
+
 		if (click == 1) {
 			if (((px - 1 == x || x == px + 1) && (py - 1 != y && y != py + 1))
 					|| ((py - 1 == y || y == py + 1) && (px - 1 != x && x != px + 1))) {
-	            try {                            // <--------------------------------------------- 네트워크
-	                writer.println(px+":"+py+":"+x+":"+y+":"+pid);
-	                writer.flush();              
-	            }
-	            catch (Exception ex) {
-	                ex.printStackTrace();
-	            } 								// <--------------------------------------------- 네트워크
-
-			
-
+				try { // <--------------------------------------------- 네트워크
+					writer.println(px + ":" + py + ":" + x + ":" + y + ":" + pid);
+					writer.flush();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} // <--------------------------------------------- 네트워크
 
 			} else {
 				JOptionPane.showMessageDialog(null, "범위 아웃");
@@ -66,52 +59,44 @@ public class Piece extends GUI {
 		} else {
 			JOptionPane.showMessageDialog(null, "말을 먼저 선택");
 			click = 0;
-		} 
-		
-		
-	
+		}
 
 	}
 
 	void kill() {
 
-	if (click == 1) {
-				if (team == turn) {
-					JOptionPane.showMessageDialog(null, "자신의 말은 잡을수 없습니다.");
-					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
-					click = 0;
-				} else if (((px - 1 == piece[id].x || piece[id].x == px + 1)
-						&& (py - 1 !=  piece[id].y &&  piece[id].y != py + 1))
-						|| ((py - 1 ==  piece[id].y ||  piece[id].y == py + 1)
-								&& (px - 1 !=  piece[id].x &&  piece[id].x != px + 1))) {
-		            try {                            // <--------------------------------------------- 네트워크
-		                writer.println(pid+":"+id);
-		                writer.flush();
-		                piece[id].killtrans();
-		            }
-		            catch (Exception ex) {
-		                ex.printStackTrace();
-		            } 								// <--------------------------------------------- 네트워크
+		if (click == 1) {
+			if (team == turn) {
+				JOptionPane.showMessageDialog(null, "자신의 말은 잡을수 없습니다.");
+				piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
+				click = 0;
+			} else if (((px - 1 == piece[id].x || piece[id].x == px + 1)
+					&& (py - 1 != piece[id].y && piece[id].y != py + 1))
+					|| ((py - 1 == piece[id].y || piece[id].y == py + 1)
+							&& (px - 1 != piece[id].x && piece[id].x != px + 1))) {
+				try { // <--------------------------------------------- 네트워크
+					writer.println(pid + ":" + id);
+					writer.flush();
+					piece[id].killtrans();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} // <--------------------------------------------- 네트워크
 
-		            click = 0;
-		            killed = 1;
-					
-		
-				
-				} else {
-					JOptionPane.showMessageDialog(null, "이동범위 벗어났습니다.");
-					piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
-					click = 0;
-				}
+				click = 0;
+				killed = 1;
 
+			} else {
+				JOptionPane.showMessageDialog(null, "이동범위 벗어났습니다.");
+				piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
+				click = 0;
 			}
-		
+
+		}
 
 	}
 
 	void trans(int id) {
-		
-	
+
 		/*if(id<7) {
 			if(count<3) {
 		piecebutton[id].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
@@ -136,80 +121,15 @@ public class Piece extends GUI {
 		deathbutton[ii].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
 		System.out.println(ii);
 		++ii;
-		kill++;
+		deathkill++;
 	}
-	if(kill==3) {
+	if(deathkill==3) {
 		JOptionPane.showMessageDialog(null, "악마 3마리를 잡으셔서 게임에서 졌습니다");
-		gaming=0;
-	}
-	
-	
-	/*if(kill==1) {
-		kill++;
-	if(state==0) {
-		System.out.println("kill 1");
-		deathbutton[0].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-	}
-	else if(state==1) {
-		System.out.println("kill 1");
-		deathbutton[0].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
+		
 	}
 }
-	else if(kill==2) {
-		kill++;
-		if(state==0) {		
-			System.out.println("kill 2");
-			deathbutton[1].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-		}
-		else if(state==1) {
-			System.out.println("kill 2");
-			deathbutton[1].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		}
-	}
-	else if(kill==3) {
-		kill++;
-		if(state==0) {			
-			System.out.println("kill 3");
-			deathbutton[2].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-		}
-		else if(state==1) {
-			System.out.println("kill 3");
-			deathbutton[2].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		}
-	}
-	else if(kill==4) {
-		kill++;
-		if(state==0) {			
-			System.out.println("kill 4");
-			deathbutton[3].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-		}
-		else if(state==1) {
-			System.out.println("kill 4");
-			deathbutton[3].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		}
-	}
-	else if(kill==5) {
-		kill++;
-		if(state==0) {		
-			System.out.println("kill 5");
-			deathbutton[4].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-		}
-		else if(state==1) {
-			System.out.println("kill 5");
-			deathbutton[4].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		}
-	}
-	else if(kill==6) {
-		kill++;
-		if(state==0) {		
-			System.out.println("kill 6");
-			deathbutton[5].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-		}
-		else if(state==1) {
-			System.out.println("kill 6");
-			deathbutton[5].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		}
-	}*/
-}
+	
+	
+
 	
 }
