b176b80d3400fe54288f248de3981b65e29791a3
HS <HS@172.26.107.154>
2019-07-11 13:31:42 +0900

    악마변경

--- Team/src/All/GUI.java
+++ Team/src/All/GUI.java
@@ -8,11 +8,11 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame {
 	static JFrame frame;
-	static JPanel board[][]=new JPanel[7][7];
+	static JPanel board[][] = new JPanel[7][7];
 	static JPanel board2[][] = new JPanel[2][3];
-	static JButton piecebutton[]=new JButton[14];
+	static JButton piecebutton[] = new JButton[14];
 	static JPanel iu;
 	static JTextArea chat;
 	static JTextField input;
@@ -23,14 +23,14 @@ public class GUI extends JFrame{
 	static JButton lockbutton;
 	static JButton unlockbutton;
 	static JButton start = new JButton("시작");
-	static JButton deathbutton[]= new JButton[5];
-	int sql=100;
-	static int click=0;
+	static JButton deathbutton[] = new JButton[5];
+	int sql = 100;
+	static int click = 0;
 	static int pid;
-	static int px,py;
+	static int px, py;
 	static int who;
 	static String pteam;
-	static String turn="P2";
+	static String turn = "P2";
 	static Piece piece[] = new Piece[14];
 	static Piece p;
 	static ChangeImage c = new ChangeImage();
@@ -38,228 +38,241 @@ public class GUI extends JFrame{
 	static ImageIcon P2icon = new ImageIcon("image/P2.png");
 	static int gaming;
 	static int killed;
-/*-----------------------------------------------------------------------------*/
-	 static JTextArea incoming;
-	 static JTextField outgoing;
-	 static BufferedReader reader;
-	 static PrintWriter writer;
-	 static Socket sock;
-	 
-/*-----------------------------------------------------------------------------*/
-		
-		
-	class StartListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
-        	gaming=1;
-			}
+	int state;
+	/*-----------------------------------------------------------------------------*/
+	static JTextArea incoming;
+	static JTextField outgoing;
+	static BufferedReader reader;
+	static PrintWriter writer;
+	static Socket sock;
+
+	/*-----------------------------------------------------------------------------*/
+
+	class StartListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
+			gaming = 1;
+		}
 	}
-	class SaveListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
-			
-			}
+
+	class SaveListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
+
+		}
 	}
-	class LoadListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
-        	
-			}
+
+	class LoadListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
+
+		}
 	}
-	class ResetListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
-        	
-			}
+
+	class ResetListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
+
+		}
 	}
-	class LockListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
+
+	class LockListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
 			c.lock();
-			}
+		}
 	}
-	class UnlockListener implements ActionListener{  //시작 전 후 를 나누는 버튼에 리스너
-		public void actionPerformed(ActionEvent e){
-        	c.unlock();
-			}
+
+	class UnlockListener implements ActionListener { // 시작 전 후 를 나누는 버튼에 리스너
+		public void actionPerformed(ActionEvent e) {
+			c.unlock();
+		}
 	}
-	
-	
-	
-	class BoardListener implements MouseListener{   
-		int x,y;
-		
-		BoardListener(int a, int b){
-             this.x=a;
-             this.y=b;
-             }
+
+	class BoardListener implements MouseListener {
+		int x, y;
+
+		BoardListener(int a, int b) {
+			this.x = a;
+			this.y = b;
+		}
+
 		public void mouseClicked(MouseEvent e) {
-			if(gaming==1) piece[pid].move(x,y); // 말이동
+			if (gaming == 1)
+				piece[pid].move(x, y); // 말이동
 			click = 0;
-   	 
-        }
+
+		}
+
 		public void mousePressed(MouseEvent e) {
-	    }
+		}
 
-	    @Override
-	    public void mouseReleased(MouseEvent e) {
-	    }
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
 
-	    @Override//
-	    public void mouseEntered(MouseEvent e) {
+		@Override //
+		public void mouseEntered(MouseEvent e) {
 
-	    }
+		}
 
-	    @Override//
-	    public void mouseExited(MouseEvent e) {
+		@Override //
+		public void mouseExited(MouseEvent e) {
 
-	    }
-}
-	class PieceListener implements ActionListener{   
-		int x, y, id; 
-		String team; 
+		}
+	}
 
-   	
-        PieceListener(int x, int y,int id,String team){
-            this.x=x;
-            this.y=y;
-            this.id=id; // ���� �ĺ���ȣ
-            this.team = team;
-       	 }
-        public void actionPerformed(ActionEvent e){
-        	
-        	if(gaming==1) piece[id].kill(); // 말잡기
-        	if(gaming==1) piece[id].postpiece(); // 말상태저장
-        	if(gaming==0) piece[id].trans(id); //말변경( 미 구현)
- 
+	class PieceListener implements ActionListener {
+		int x, y, id;
+		String team;
+		int state;
+
+		PieceListener(int x, int y, int id, int state, String team) {
+			this.x = x;
+			this.y = y;
+			this.id = id; // ���� �ĺ���ȣ
+			this.team = team;
+			this.state = state;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+
+			if (gaming == 1)
+				piece[id].kill(); // 말잡기
+			if (gaming == 1)
+				piece[id].postpiece(); // 말상태저장
+
+			if (gaming == 0 && Piece.count > 0) {
+
+				piece[id].trans(id); // 말변경( 미 구현)
+
+				if (piece[id].state == 0) {
+					piece[id].state = 1;
+					Piece.count--;
+					System.out.println("1로변경");
+				} else if (piece[id].state == 1) {
+					JOptionPane.showMessageDialog(null, "딴거 눌러 시발련아.");
+
+				}
+
 			}
-			 	}
 
-	
-    public class SendButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent ev) {
-            try {
-                writer.println(outgoing.getText());
-                writer.flush();
-                
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-            outgoing.setText("");
-            outgoing.requestFocus();
-        }
-    }
-        
-	
-	
+		}
+
+	}
+
+	public class SendButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			try {
+				writer.println(outgoing.getText());
+				writer.flush();
+
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+			outgoing.setText("");
+			outgoing.requestFocus();
+		}
+	}
+
 	public void addboard() {
-		int aa=0;
-		for(int x=0; x<7; x++) {
+		int aa = 0;
+		for (int x = 0; x < 7; x++) {
 
-			for(int y=0; y<7; y++) {
-				if(x<2) {
-					if(y<3) {
-				board2[x][y]=new JPanel();
-				board2[x][y].setBackground(Color.black);
-				board2[x][y].setLocation((y+8) * 90,(x+8) * 90); 
-				board2[x][y].setSize(90, 90);
-				board2[x][y].setLayout(new FlowLayout());
-				frame.add(board2[x][y]);
+			for (int y = 0; y < 7; y++) {
+				if (x < 2) {
+					if (y < 3) {
+						board2[x][y] = new JPanel();
+						board2[x][y].setBackground(Color.black);
+						board2[x][y].setLocation((y + 8) * 90, (x + 8) * 90);
+						board2[x][y].setSize(90, 90);
+						board2[x][y].setLayout(new FlowLayout());
+						frame.add(board2[x][y]);
 					}
 				}
 				board[x][y] = new JPanel();
-				if(x%2==0&&y%2==0) {
+				if (x % 2 == 0 && y % 2 == 0) {
 					board[x][y].setBackground(Color.black);
-				}else if(x%2!=0&&y%2==0) {
+				} else if (x % 2 != 0 && y % 2 == 0) {
 					board[x][y].setBackground(Color.white);
-				}else if(x%2==0&&y%2!=0) {
+				} else if (x % 2 == 0 && y % 2 != 0) {
 					board[x][y].setBackground(Color.white);
-				}else if(x%2!=0&&y%2!=0) {
+				} else if (x % 2 != 0 && y % 2 != 0) {
 					board[x][y].setBackground(Color.black);
 				}
-				if(x==0) {
+				if (x == 0) {
 					piecebutton[aa].setIcon(P1icon);
 					board[x][y].add(piecebutton[aa]);
-					piecebutton[aa].addActionListener(new PieceListener(x,y,aa,"P1"));
-					piece[aa] = new Piece(x, y, aa,0,"P1");
+					piecebutton[aa].addActionListener(new PieceListener(x, y, aa, state, "P1"));
+					piece[aa] = new Piece(x, y, aa, 0, "P1");
 					aa++;
 				}
-				if(x==6) {
+				if (x == 6) {
 					piecebutton[aa].setIcon(P2icon);
 					board[x][y].add(piecebutton[aa]);
-					piecebutton[aa].addActionListener(new PieceListener(x,y,aa,"P2"));
-					piece[aa] = new Piece(x, y, aa,0,"P2");
+					piecebutton[aa].addActionListener(new PieceListener(x, y, aa, state, "P2"));
+					piece[aa] = new Piece(x, y, aa, 0, "P2");
 					aa++;
 				}
-				board[x][y].setLocation(y * sql,x* sql+40); 
+				board[x][y].setLocation(y * sql, x * sql + 40);
 				board[x][y].setSize(sql, sql);
 				board[x][y].setLayout(new FlowLayout());
-				board[x][y].addMouseListener(new BoardListener(x,y));
-				
-				frame.add(BorderLayout.CENTER,board[x][y]);
+				board[x][y].addMouseListener(new BoardListener(x, y));
+
+				frame.add(BorderLayout.CENTER, board[x][y]);
 			}
 		}
 		start.addActionListener(new StartListener());
 
-		
-		
 		addgui();
 	}
-	
+
 	public void addpiece() {
 		frame = new JFrame();
 		frame.setLayout(new BorderLayout());
-		
-		for(int i=0; i<14; i++) {
+
+		for (int i = 0; i < 14; i++) {
 			piecebutton[i] = new JButton();
 			piecebutton[i].setPreferredSize(new Dimension(80, 80));
-			piecebutton[i].setBorderPainted(false); 
-			piecebutton[i].setFocusPainted(false);  
+			piecebutton[i].setBorderPainted(false);
+			piecebutton[i].setFocusPainted(false);
 			piecebutton[i].setContentAreaFilled(false);
-			
-			
-			
-			
-			
-			
-		
-	}
+
+		}
 		addboard();
 
-}
-	
+	}
+
 	public void addgui() {
 		/*---------------------------------------------------------------------*/
-        incoming = new JTextArea();
-        incoming.setLineWrap(true);
-        incoming.setWrapStyleWord(true);
-        incoming.setEditable(false);
-        JScrollPane qScroller = new JScrollPane(incoming);
-        qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        outgoing = new JTextField();
-        JButton sendButton = new JButton("Send");
-        sendButton.addActionListener(new SendButtonListener());
-        qScroller.setBounds(800, 100, 350, 200);
-        outgoing.setBounds(800, 300, 250, 40);
-        sendButton.setBounds(1050, 300, 100, 40);
-        frame.add(qScroller);
-        frame.add(outgoing);
-        frame.add(sendButton);
-        /*--------------------------------------------------------------------------------------*/
+		incoming = new JTextArea();
+		incoming.setLineWrap(true);
+		incoming.setWrapStyleWord(true);
+		incoming.setEditable(false);
+		JScrollPane qScroller = new JScrollPane(incoming);
+		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		outgoing = new JTextField();
+		JButton sendButton = new JButton("Send");
+		sendButton.addActionListener(new SendButtonListener());
+		qScroller.setBounds(800, 100, 350, 200);
+		outgoing.setBounds(800, 300, 250, 40);
+		sendButton.setBounds(1050, 300, 100, 40);
+		frame.add(qScroller);
+		frame.add(outgoing);
+		frame.add(sendButton);
+		/*--------------------------------------------------------------------------------------*/
 		savebutton = new JButton("save");
 		loadbutton = new JButton("load");
 		resetbutton = new JButton("reset");
 		lockbutton = new JButton("lock");
-		unlockbutton= new JButton("unloack");
-		
+		unlockbutton = new JButton("unloack");
+
 		savebutton.addActionListener(new SaveListener());
 		loadbutton.addActionListener(new LoadListener());
 		resetbutton.addActionListener(new ResetListener());
 		lockbutton.addActionListener(new LockListener());
 		unlockbutton.addActionListener(new UnlockListener());
-		
-		
-		iu= new JPanel();
+
+		iu = new JPanel();
 		iu.setLayout(null);
 		iu.setBackground(Color.GRAY);
-	
+
 		savebutton.setBounds(900, 100, 100, 40);
 		loadbutton.setBounds(900, 140, 100, 40);
 		resetbutton.setBounds(900, 180, 100, 40);
@@ -271,17 +284,13 @@ public class GUI extends JFrame{
 		frame.setSize(1200, 1200);
 		frame.setTitle("Angel and Devil Game");
 		frame.setVisible(true);
-		
-		
+
 	}
-	
 
-	
 	public static void main(String[] args) {
-			new ClientNetworking().go();
-			GUI gui=new GUI();
-			gui.addpiece();
+		new ClientNetworking().go();
+		GUI gui = new GUI();
+		gui.addpiece();
 
-		
 	}
 }
--- Team/src/All/Piece.java
+++ Team/src/All/Piece.java
@@ -7,14 +7,16 @@ public class Piece extends GUI {
 	int x, y, id, state;
 	String team;
 	int ascore, dscore;
-	static int count=0,count2=0;
-	Piece(int x,int y,int id,int state,String team) {
+	static int count = 3, count2 = 0;
+
+	Piece(int x, int y, int id, int state, String team) {
 		this.x = x;
 		this.y = y;
 		this.id = id;
-		this.state=state;
+		this.state = state;
 		this.team = team;
 	}
+
 	void postpiece() {
 		if (click == 0) {
 			if (team == turn) {
@@ -22,41 +24,39 @@ public class Piece extends GUI {
 				py = this.y;
 				pid = this.id;
 				pteam = this.team;
-				click++;	
+				click++;
+				if (state == 1) {
+					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + "_Devil" + "_clik.png"));
+				}
 				piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + "_clik.png"));
 
-
-				
-			}else if(killed==1){
+			} else if (killed == 1) {
 				killed = 0;
-				
-			}else {
+
+			} else {
 				JOptionPane.showMessageDialog(null, "순서가 아닙니다.");
 			}
-    	}
-
-
+		}
 
 	}
 
 	public void move(int x, int y) {
-		
+
 		if (click == 1) {
 			if (((px - 1 == x || x == px + 1) && (py - 1 != y && y != py + 1))
 					|| ((py - 1 == y || y == py + 1) && (px - 1 != x && x != px + 1))) {
-	            try {                            // <--------------------------------------------- 네트워크
-	                writer.println(px+":"+py+":"+x+":"+y+":"+pid);
-	                writer.flush();              
-	            }
-	            catch (Exception ex) {
-	                ex.printStackTrace();
-	            } 								// <--------------------------------------------- 네트워크
-
-			
-
+				try { // <--------------------------------------------- 네트워크
+					writer.println(px + ":" + py + ":" + x + ":" + y + ":" + pid + ":" + state);
+					writer.flush();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} // <--------------------------------------------- 네트워크
 
 			} else {
 				JOptionPane.showMessageDialog(null, "범위 아웃");
+				if (state == 1) {
+					piecebutton[pid].setIcon(new ImageIcon("image/" + turn + "_Devil" + ".png"));
+				}
 				piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
 				click = 0;
 
@@ -65,63 +65,56 @@ public class Piece extends GUI {
 		} else {
 			JOptionPane.showMessageDialog(null, "말을 먼저 선택");
 			click = 0;
-		} 
-		
-		
-	
+		}
 
 	}
 
 	void kill() {
 
-	if (click == 1) {
-				if (team == turn) {
-					JOptionPane.showMessageDialog(null, "자신의 말은 잡을수 없습니다.");
-					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
-					click = 0;
-				} else if (((px - 1 == piece[id].x || piece[id].x == px + 1)
-						&& (py - 1 !=  piece[id].y &&  piece[id].y != py + 1))
-						|| ((py - 1 ==  piece[id].y ||  piece[id].y == py + 1)
-								&& (px - 1 !=  piece[id].x &&  piece[id].x != px + 1))) {
-		            try {                            // <--------------------------------------------- 네트워크
-		                writer.println(pid+":"+id);
-		                writer.flush();              
-		            }
-		            catch (Exception ex) {
-		                ex.printStackTrace();
-		            } 								// <--------------------------------------------- 네트워크
-
-		            click = 0;
-		            killed = 1;
-					
-		
-				
-				} else {
-					JOptionPane.showMessageDialog(null, "이동범위 벗어났습니다.");
-					piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
-					click = 0;
+		if (click == 1) {
+			if (team == turn) {
+				JOptionPane.showMessageDialog(null, "자신의 말은 잡을수 없습니다.");
+				if (state == 1) {
+					piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + "_Devil" + ".png"));
 				}
+				piecebutton[pid].setIcon(new ImageIcon("image/" + pteam + ".png"));
+				click = 0;
+			} else if (((px - 1 == piece[id].x || piece[id].x == px + 1)
+					&& (py - 1 != piece[id].y && piece[id].y != py + 1))
+					|| ((py - 1 == piece[id].y || piece[id].y == py + 1)
+							&& (px - 1 != piece[id].x && piece[id].x != px + 1))) {
+				try { // <--------------------------------------------- 네트워크
+					writer.println(pid + ":" + id);
+					writer.flush();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				} // <--------------------------------------------- 네트워크
 
+				click = 0;
+				killed = 1;
+
+			} else {
+				JOptionPane.showMessageDialog(null, "이동범위 벗어났습니다.");
+
+				if (state == 1) {
+					piecebutton[pid].setIcon(new ImageIcon("image/" + turn + "_Devil" + ".png"));
+				}
+				piecebutton[pid].setIcon(new ImageIcon("image/" + turn + ".png"));
+				click = 0;
 			}
-		
+
+		}
 
 	}
-
+	void win(int id, int x, int y) {
+		
+	}
+	
 	void trans(int id) {
 		
-	
-		/*if(id<7) {
-			if(count<3) {
-		piecebutton[id].setIcon(new ImageIcon("image/" + "Devil" + ".png"));
-		count++;
-			}
-		}else {
-			if(count2<3) {
-			piecebutton[id].setIcon(new ImageIcon("image/" + "Angel" + ".png"));
-			count2++;
-			}
-		}*/
+		piecebutton[id].setIcon(new ImageIcon("image/" + turn + "_Devil" + ".png"));
 		
-
+		
 	}
+
 }
